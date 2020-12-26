3e3e59a4e10576b29125cf850463053eb1e899ef
biad-iritie <biadboze@gmail.com>
2020-12-26 16:16:48 +0800

    Build folder

---
+++ build/precache-manifest.f29468c019612f23ee522b4f007a13d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8aeeee588f908ed9388517aaa0eb501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aafc7336b948437a6206",
+    "url": "/static/css/2.744cd6b5.chunk.css"
+  },
+  {
+    "revision": "366081103699f165a143",
+    "url": "/static/css/main.fe769619.chunk.css"
+  },
+  {
+    "revision": "aafc7336b948437a6206",
+    "url": "/static/js/2.6bab98cf.chunk.js"
+  },
+  {
+    "revision": "13d1b07647e807c67da00fac386f08bd",
+    "url": "/static/js/2.6bab98cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75027921f407c905742d",
+    "url": "/static/js/3.2494b987.chunk.js"
+  },
+  {
+    "revision": "48763b73d8c195527ebddd5857148be0",
+    "url": "/static/js/3.2494b987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2754a0bc22dd95f053d",
+    "url": "/static/js/4.6d2869b8.chunk.js"
+  },
+  {
+    "revision": "cb360104aa1bc33bcdba",
+    "url": "/static/js/5.862effc2.chunk.js"
+  },
+  {
+    "revision": "366081103699f165a143",
+    "url": "/static/js/main.012aeca6.chunk.js"
+  },
+  {
+    "revision": "f8bbaf16316b29b18bed",
+    "url": "/static/js/runtime-main.8ebbb4a1.js"
+  }
+]);
